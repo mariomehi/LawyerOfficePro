a5ecd6ce5d93802f2a49563e47f2a5e640b7406f
Marien Mehillka <58168890+mariomehi@users.noreply.github.com>
2022-05-17 09:25:49 +0200

    Add files via upload

---
+++ Utility/SigData.cs
@@ -0,0 +1,47 @@
+﻿using LawyerOffice.Contracts;
+using LawyerOffice.Implementation;
+using System.Collections.Generic;
+
+namespace LawyerOffice.Service
+{
+    public static class SigData
+    {
+        public static List<Restaurant> Init()
+        {
+            List<Food> f0 = new List<Food>() {
+                new Food("Cappuccino"),
+                new Food("Espresso"),
+                new Food("Macchiatto") };
+            List<Food> f1 = new List<Food>() {
+                new Food("Apollo"),
+                new Food("Camogli"),
+                new Food("Tonno") };
+            List<Food> f2 = new List<Food>() {
+                new Food("Margherita"),
+                new Food("Boscaiola"),
+                new Food("Capricciosa") };
+
+            Dictionary<FOODTYPE, List<Food>> d1 = new Dictionary<FOODTYPE, List<Food>>();
+            d1.Add(FOODTYPE.Coffee, f0);
+            d1.Add(FOODTYPE.Sandwich, f1);
+            d1.Add(FOODTYPE.Pizza, f2);
+
+            Dictionary<FOODTYPE, List<Food>> d2 = new Dictionary<FOODTYPE, List<Food>>();
+            d2.Add(FOODTYPE.Coffee, f0);
+
+            Dictionary<FOODTYPE, List<Food>> d3 = new Dictionary<FOODTYPE, List<Food>>();
+            d3.Add(FOODTYPE.Sandwich, f1);
+            d3.Add(FOODTYPE.Pizza, f2);
+
+
+            List<Restaurant> ristoranti = new List<Restaurant>();
+            ristoranti.Add(new Restaurant("Starbucks", RestaurantType.breakfast, d2));
+            ristoranti.Add(new Restaurant("McDonalds", RestaurantType.launch, d1));
+            ristoranti.Add(new Restaurant("BurgerKing", RestaurantType.dinner, d3));
+
+            return ristoranti;
+
+        }
+
+    }
+}
